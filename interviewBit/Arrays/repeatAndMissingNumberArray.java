03f7098aaac25d0bd634e0ba86e92de29da48d2a
Sourabh Kondapaka <sourabhkondapaka@gmail.com>
2019-06-07 12:04:21 +0530

    Added array files

---
+++ interviewBit/Arrays/repeatAndMissingNumberArray.java
@@ -0,0 +1,35 @@
+    public ArrayList<Integer> repeatedNumber(final List<Integer> A) 
+    {
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        
+        
+        int current = 0;
+        for(int i =0; i < A.size();i++)
+        {
+            current = A.get(i);
+            if(!map.containsKey(current))
+                map.put(current,1);
+            else
+                {
+                    map.put(current, map.get(current) + 1 );
+                    result.add(current);
+                }
+        }
+        
+        for(int i =1; i <= A.size(); i++)
+        {
+            if(!map.containsKey(i))
+                {
+                    result.add(i);
+                    break;
+                }
+                
+        }
+        
+        
+        
+        return result;
+    }
